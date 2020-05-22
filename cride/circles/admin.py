ef67b8caf801b9cc187babb9485b1a2ba1d42646
mariogonzcardona <alejandrogonzalez@uadec.edu.mx>
2020-05-22 18:25:33 -0500

    Se agrego Users, Profile y Circles a Admin

---
+++ cride/circles/admin.py
@@ -0,0 +1,26 @@
+"""Circles Admin"""
+
+#Django
+from django.contrib import admin
+
+# Model
+from cride.circles.models import Circle
+
+@admin.register(Circle)
+class CircleAdmin(admin.ModelAdmin):
+    """Circle Admin"""
+
+    list_display=(
+        'slug_name',
+        'name',
+        'is_public',
+        'verified',
+        'members_limit'
+    )
+
+    search_fields=('slug_name','name')
+    list_filter=(
+        'is_public',
+        'verified',
+        'members_limit'
+    )
---
+++ cride/circles/models/circles.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+from cride.utils.models import CRideModel
+
+class Circle(CRideModel):
+    """Circle model"""
+
+    name=models.CharField('circle name', max_length=140)
+    slug_name=models.SlugField(unique=True,max_length=40)
+    about=models.CharField('circle description', max_length=255)
+    picture=models.ImageField(upload_to='circles/pictures', blank=True,null=True)
+
+    #Stats
+    rides_offered=models.PositiveIntegerField(default=0)
+    rides_taken=models.PositiveIntegerField(default=0)
+
+    verified=models.BooleanField(
+        'verified circle',
+        default=True,
+        help_text='Verified circles are also know as official communities.'
+    )
+
+    is_public=models.BooleanField(
+        default=True,
+        help_text='Public circles are listed in the main page so every konw about their existence.'
+    )
+
+    is_limited=models.BooleanField(
+        'limited',
+        default=True,
+        help_text='Limited circles can grow up to a fixed number of members'
+    )
+    members_limit=models.PositiveIntegerField(
+        default=0,
+        help_text='If circle is limited, this will be the limit on the number of members.'
+    )
+
+    def __str__(self):
+        return self.name
+    
+    class Meta(CRideModel.Meta):
+        ordering=['-rides_taken','-rides_offered']
+
+    
+    
